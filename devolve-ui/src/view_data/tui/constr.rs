1fdcf887fad47ee60176a4edbbb14d513cba64ce
jakobeha <jakobeha@fastmail.com>
2022-06-07 03:15:45 -0400

    @WIP added missing view macros

--- devolve-ui/src/view_data/tui/constr.rs
+++ devolve-ui/src/view_data/tui/constr.rs
@@ -5,7 +5,8 @@ use crate::core::view::color::Color;
 use crate::core::view::constr::{constr_view, VViewConstrArgs};
 use crate::core::view::layout::bounds::Measurement;
 use crate::core::view::layout::parent_bounds::{LayoutDirection, SubLayout};
-use crate::view_data::attrs::TextWrapMode;
+use crate::view_data::attrs::{BorderStyle, DividerDirection, DividerStyle, TextWrapMode};
+use crate::view_data::tui::terminal_image::{HandleAspectRatio, Source};
 use crate::view_data::tui::tui::TuiViewData;
 
 #[derive(Default)]
@@ -21,6 +22,21 @@ pub struct TextConstrArgs {
     pub color: Option<Color>,
     pub wrap: TextWrapMode,
     pub text: String
+}
+
+#[derive(Default)]
+pub struct BorderConstrArgs {
+    pub color: Option<Color>
+}
+
+#[derive(Default)]
+pub struct DividerConstrArgs {
+    pub color: Option<Color>
+}
+
+#[derive(Default)]
+pub struct SourceConstrArgs {
+    pub handle_aspect_ratio: HandleAspectRatio
 }
 
 macro _box2(($d:tt) @ $name:ident, $layout_direction: expr) {
@@ -108,3 +124,85 @@ pub macro ptext({ $( $view_field:ident: $view_value:expr ),* }, { $( $data_field
     })
 }
 
+pub fn color(view_args: VViewConstrArgs, color: Color) -> VNode<TuiViewData> {
+    constr_view(view_args, TuiViewData::Color {
+        color
+    })
+}
+
+pub macro color({ $( $view_field:ident: $view_value:expr ),* }, $color:expr) {
+    color(VViewConstrArgs {
+        $( $view_field : $view_value, )*
+        ..VViewConstrArgs::default()
+    }, $color)
+}
+
+pub fn border(view_args: VViewConstrArgs, data_args: BorderConstrArgs, style: BorderStyle) -> VNode<TuiViewData> {
+    constr_view(view_args, TuiViewData::Border {
+        color: data_args.color,
+        style
+    })
+}
+
+pub macro border({ $( $view_field:ident: $view_value:expr ),* }, { $( data_field:ident: $data_value:expr ),* }, $style:expr) {
+    border(VViewConstrArgs {
+        $( $view_field : $view_value, )*
+        ..VViewConstrArgs::default()
+    }, BorderConstrArgs {
+        $( $data_field : $data_value, )*
+        ..BorderConstrArgs::default()
+    }, $style)
+}
+
+pub fn hdivider(view_args: VViewConstrArgs, data_args: BorderConstrArgs, style: DividerStyle) -> VNode<TuiViewData> {
+    constr_view(view_args, TuiViewData::Divider {
+        color: data_args.color,
+        direction: DividerDirection::Horizontal,
+        style
+    })
+}
+
+pub macro hdivider({ $( $view_field:ident: $view_value:expr ),* }, { $( data_field:ident: $data_value:expr ),* }, $style:expr) {
+    hdivider(VViewConstrArgs {
+        $( $view_field : $view_value, )*
+        ..VViewConstrArgs::default()
+    }, DividerConstrArgs {
+        $( $data_field : $data_value, )*
+        ..DividerConstrArgs::default()
+    }, $style)
+}
+
+pub fn vdivider(view_args: VViewConstrArgs, data_args: BorderConstrArgs, style: DividerStyle) -> VNode<TuiViewData> {
+    constr_view(view_args, TuiViewData::Divider {
+        color: data_args.color,
+        direction: DividerDirection::Vertical,
+        style
+    })
+}
+
+pub macro vdivider({ $( $view_field:ident: $view_value:expr ),* }, { $( data_field:ident: $data_value:expr ),* }, $style:expr) {
+    vdivider(VViewConstrArgs {
+        $( $view_field : $view_value, )*
+        ..VViewConstrArgs::default()
+    }, DividerConstrArgs {
+        $( $data_field : $data_value, )*
+        ..DividerConstrArgs::default()
+    }, $style)
+}
+
+pub fn source(view_args: VViewConstrArgs, data_args: SourceConstrArgs, source: Source) -> VNode<TuiViewData> {
+    constr_view(view_args, TuiViewData::Source {
+        handle_aspect_ratio: data_args.handle_aspect_ratio,
+        source
+    })
+}
+
+pub macro source({ $( $view_field:ident: $view_value:expr ),* }, { $( data_field:ident: $data_value:expr ),* }, $source:expr) {
+    source(VViewConstrArgs {
+        $( $view_field : $view_value, )*
+        ..VViewConstrArgs::default()
+    }, SourceConstrArgs {
+        $( $data_field : $data_value, )*
+        ..SourceConstrArgs::default()
+    }, $source)
+}
