9ff525412c7fe861f9ea2a737cc5814be79ec10a
jakobeha <jakobeha@gmail.com>
2022-06-13 23:10:54 -0400

    refactor context (passes readme test)

--- devolve-ui/tests/readme.rs
+++ devolve-ui/tests/readme.rs
@@ -15,6 +15,9 @@ use std::path::PathBuf;
 use std::time::Duration;
 #[allow(unused_imports)] // Needed for IntelliJ macro expansion
 use devolve_ui::core::component::constr::{_make_component, _make_component2, make_component};
+use devolve_ui::core::component::constr::make_component2;
+use devolve_ui::core::component::context::VComponentContext2;
+use devolve_ui::core::component::node::VNode;
 use devolve_ui::core::renderer::renderer::{Renderer, RendererOverrides};
 use devolve_ui::core::hooks::state::use_state;
 use devolve_ui::core::view::color::Color;
@@ -27,48 +30,43 @@ use devolve_ui::view_data::attrs::BorderStyle;
 use devolve_ui::view_data::tui::constr::{border, hbox, source, text, zbox};
 use devolve_ui::core::hooks::event::{CallFirst, use_interval};
 
-make_component!(
-    header,
-    HeaderProps {
-        name: String
-    },
-    {
-        name: Default::default()
-    },
-    <TuiViewData>|c, name| {
-        let counter = use_state(c, || 0);
-        use_interval(c, Duration::from_secs(1), CallFirst::AfterTheInterval, move |c| {
-            *counter.get_mut(c) += 1;
-        });
+#[derive(Default)]
+pub struct HeaderProps {
+    name: String
+}
 
-        zbox!({ width: smt!(100%) }, {}, vec![
-            zbox!({ x: mt!(1), y: mt!(1), width: smt!(32) }, {}, vec![
-                text!({}, { color: Some(Color::yellow()) }, format!("Hello {}", name)),
-                text!({ x: mt!(100%), anchor_x: 1f32 }, { color: Some(Color::yellow()) }, format!("{} seconds", counter.get(c)))
-            ]),
-            border!({ width: smt!(34), height: smt!(prev + 2) }, { color: Some(Color::yellow()) }, BorderStyle::Rounded)
-        ])
-    }
-);
+#[derive(Default)]
+pub struct ReadmeProps {
+    name: String
+}
 
-make_component!(
-    pub readme,
-    ReadmeProps {
-        name: String
-    },
-    {
-        name: Default::default()
-    },
-    <TuiViewData>|c, name| {
-        zbox!({ width: smt!(100%) }, {}, vec![
-            hbox!({ x: mt!(2), y: mt!(1), width: smt!(100% - 4) }, { gap: mt!(1) }, vec![
-                header!(c, "header", { name: name.clone() }),
-                source!({ width: smt!(34), height: smt!(16) }, {}, Source::Path(PathBuf::from("assets/dog.png")))
-            ]),
-            border!({ width: smt!(100%), height: smt!(prev + 2) }, { color: Some(Color::blue()) }, BorderStyle::Rounded)
-        ])
-    }
-);
+pub fn header((mut c, HeaderProps { name }): VComponentContext2<HeaderProps, TuiViewData>) -> VNode<TuiViewData> {
+    let counter = use_state(&mut c, || 0);
+    use_interval(&mut c, Duration::from_secs(1), CallFirst::AfterTheInterval, move |(mut c, HeaderProps { .. })| {
+        *counter.get_mut(&mut c) += 1;
+    });
+
+    zbox!({ width: smt!(100%) }, {}, vec![
+        zbox!({ x: mt!(1), y: mt!(1), width: smt!(32) }, {}, vec![
+            text!({}, { color: Some(Color::yellow()) }, format!("Hello {}", name)),
+            text!({ x: mt!(100%), anchor_x: 1f32 }, { color: Some(Color::yellow()) }, format!("{} seconds", counter.get(&mut c)))
+        ]),
+        border!({ width: smt!(34), height: smt!(prev + 2) }, { color: Some(Color::yellow()) }, BorderStyle::Rounded)
+    ])
+}
+
+pub fn readme((mut c, ReadmeProps { name }): VComponentContext2<ReadmeProps, TuiViewData>) -> VNode<TuiViewData> {
+    zbox!({ width: smt!(100%) }, {}, vec![
+        hbox!({ x: mt!(2), y: mt!(1), width: smt!(100% - 4) }, { gap: mt!(1) }, vec![
+            header!(&mut c, "header", { name: name.clone() }),
+            source!({ width: smt!(34), height: smt!(16) }, {}, Source::Path(PathBuf::from("assets/dog.png")))
+        ]),
+        border!({ width: smt!(100%), height: smt!(prev + 2) }, { color: Some(Color::blue()) }, BorderStyle::Rounded)
+    ])
+}
+
+make_component2!(pub header, HeaderProps);
+make_component2!(pub readme, ReadmeProps);
 
 struct TestOutput {
     buf: Rc<RefCell<Vec<u8>>>
@@ -119,7 +117,7 @@ fn test_render() {
         ignore_events: true,
         ..RendererOverrides::default()
     });
-    renderer.root(|c| readme!(c, "readme", { name: "devolve-ui".into() }));
+    renderer.root(|(mut c, ())| readme!(&mut c, "readme", { name: "devolve-ui".into() }));
     // renderer.interval_between_frames(Duration::from_millis(25)); // optional
     renderer.show();
     // renderer.resume();
